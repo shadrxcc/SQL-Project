80444c24034367e06376ed9924d2d8f27b426bf8
shadrach akaade <akaadesm@gmail.com>
2022-01-17 03:11:57 +0100

    query to find the country that starts with Y

--- query.sql
+++ query.sql
@@ -1,6 +1,7 @@
-SELECT population FROM world
-  WHERE name = 'Germany';
+SELECT population FROM world WHERE name = 'Germany';
 
-  SELECT name, population FROM world WHERE name IN ('Sweden', 'Norway', 'Denmark');
+SELECT name, population FROM world WHERE name IN ('Sweden', 'Norway', 'Denmark');
 
-  SELECT name, area FROM world WHERE area BETWEEN 200000 AND 250000;
+SELECT name, area FROM world WHERE area BETWEEN 200000 AND 250000;
+
+SELECT name FROM world WHERE name LIKE 'Y%';
