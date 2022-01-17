a650f3556c74fc2b6499108f8510bc9f913be80b
shadrach akaade <akaadesm@gmail.com>
2022-01-18 00:58:40 +0100

    query to list the name and continent of countries in the continents containing either Argentina or Australia. Order by name of the country.

--- query.sql
+++ query.sql
@@ -76,3 +76,4 @@ SELECT name FROM world WHERE population > (SELECT population FROM world WHERE na
 
 SELECT name FROM world WHERE continent = 'Europe' AND  gdp/population > (SELECT gdp/population FROM world WHERE name = 'United Kingdom')
 
+SELECT name, continent FROM world WHERE continent IN (SELECT continent FROM world WHERE name = 'Argentina' OR name = 'Australia') ORDER BY name
