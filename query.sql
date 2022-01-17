479ec7fb9d65b9bb34641438f1cac436bd20740f
shadrach akaade <akaadesm@gmail.com>
2022-01-18 00:59:52 +0100

    query to show population that is more than Canada but less than Poland

--- query.sql
+++ query.sql
@@ -77,3 +77,5 @@ SELECT name FROM world WHERE population > (SELECT population FROM world WHERE na
 SELECT name FROM world WHERE continent = 'Europe' AND  gdp/population > (SELECT gdp/population FROM world WHERE name = 'United Kingdom')
 
 SELECT name, continent FROM world WHERE continent IN (SELECT continent FROM world WHERE name = 'Argentina' OR name = 'Australia') ORDER BY name
+
+SELECT name, population FROM world WHERE population > (SELECT population FROM world WHERE name = 'Canada') AND population < (SELECT population FROM world WHERE name = 'Poland')
