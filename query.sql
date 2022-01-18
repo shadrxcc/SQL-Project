f204c1b72d51fc455f69f2b5d6a10475fa105816
shadrach akaade <akaadesm@gmail.com>
2022-01-18 01:20:58 +0100

    each continent showing the continent and number of countries with populations of at least 10 million.

--- query.sql
+++ query.sql
@@ -106,3 +106,4 @@ SELECT SUM(population) FROM world WHERE name IN ('Estonia', 'Latvia', 'Lithuania
 
 SELECT continent, COUNT(name) FROM world GROUP BY continent;
 
+SELECT continent, COUNT(name) FROM world WHERE population >= 10000000 GROUP BY continent;
