7a53202d48c490d863162cb557950f593342a4ae
shadrach akaade <akaadesm@gmail.com>
2022-01-18 01:00:39 +0100

    query to show the name and the population of each country in Europe. Show the population as a percentage of the population of Germany

--- query.sql
+++ query.sql
@@ -79,3 +79,6 @@ SELECT name FROM world WHERE continent = 'Europe' AND  gdp/population > (SELECT 
 SELECT name, continent FROM world WHERE continent IN (SELECT continent FROM world WHERE name = 'Argentina' OR name = 'Australia') ORDER BY name
 
 SELECT name, population FROM world WHERE population > (SELECT population FROM world WHERE name = 'Canada') AND population < (SELECT population FROM world WHERE name = 'Poland')
+
+SELECT name, CONCAT(ROUND(population/(SELECT population FROM world WHERE name='Germany')*100, 0),'%') FROM world WHERE continent='Europe'
+
