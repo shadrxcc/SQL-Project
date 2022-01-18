9869af0bda2f835e1d8d37b765e62eda5e038b15
shadrach akaade <akaadesm@gmail.com>
2022-01-18 01:18:44 +0100

    countries that have an area of at least 1000000

--- query.sql
+++ query.sql
@@ -99,3 +99,5 @@ SELECT SUM(population) FROM world;
 SELECT DISTINCT continent FROM world;
 
 SELECT SUM(gdp) FROM world WHERE continent = 'Africa';
+
+SELECT COUNT(*) FROM world WHERE area >= 1000000;
